f965fc40ef9e3e434431a32983fd940eee1cc299
tarique313 <mahmud.tarique@gmail.com>
2014-06-30 10:43:55 +0600

    configure routs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Csc498::Application.config.secret_token = 'd319767fee867fdaad3a23269d5de268c6ff9cae3fc8125e6ae126fe0af153367571d62f3d7ba0acde49429d7879be6cac38a04974cbfbc081af00418959ba8b'
